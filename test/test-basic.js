de7cfcc40e7d45f3a637e47b30c4c19722b1ca9a
Henry <henry.cooke@bbc.co.uk>
2018-01-12 23:00:20 +0000

    added test case for rendered speech urls

--- test/test-basic.js
+++ test/test-basic.js
@@ -1,8 +1,8 @@
 var chai = require('chai');
 chai.use(require('chai-json-schema'));
+chai.use(require('chai-url'));
 
-//var should = chai.should();
-var assert = chai.assert;
+var expect = chai.expect;
 
 var PollyS3 = require( "../src/polly-s3" );
 
@@ -39,19 +39,25 @@ describe('Basic tests', function() {
   
   it('should list ALL voices', function(done){
     
-    var p = new PollyS3({
-        region: "eu-west-1"
-    });
+    var p = new PollyS3();
     
     p.describeVoices( function( err, data ){
-        if( err ){
-            throw( err );
-        } else if( data ){
-            // TODO: check data is structured as expected
-            console.log( data );
-            assert.jsonSchema( data, schemaVoiceList );
-            done();    
-        }
+        if( err ) throw( err );
+        expect( data ).to.be.jsonSchema( schemaVoiceList );
+        done();
+    });
+  });
+
+  it('should return a URL for a call to renderSentence', function(done){
+
+    var p = new PollyS3();
+    var sentence = "Is this thing on?";
+
+    p.renderSentence( sentence, function( err, url ){
+        if( err ) throw(err);
+        expect( url ).to.contain.hostname( 'amazonaws.com' );
+        expect( url ).to.contain.path( 'mp3' );
+        done();
     });
   });
 
